6fd8a2721ca4e4645d6b528e1093747e4c7cf4ec
HassanMunene <awanzihassan@gmail.com>
2023-04-06 14:32:23 +0300

    adding a docstrign about the class FileStorage

---
+++ models/file_storage.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""
+This module is about creating a class called FileStorage
+This class will be used to store and retrieve all the instances of
+classes created in this project
+They will be stored in form of a json string.
+It will do the serialization and deserialization of the instances
+"""
