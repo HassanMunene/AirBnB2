9f1b4af32c55ef512c35e8f49457294cefbb26fa
HassanMunene <awanzihassan@gmail.com>
2023-04-06 14:49:25 +0300

    more about we are creating in the docstring

--- models/file_storage.py
+++ models/file_storage.py
@@ -5,4 +5,19 @@ This class will be used to store and retrieve all the instances of
 classes created in this project
 They will be stored in form of a json string.
 It will do the serialization and deserialization of the instances
+To acheive this the class will have two private attributes:
+    - __file_path: will store the path to the json file
+    - __objects: will store the objects we want to save to the json file
+The class too has some methods:
+    - all(self): the function of this is to return the __objects
+    - new(self, obj): add an object to the dictionary __objects
+    - save(self): After adding the object this func will serealize all the objects in the __objects dictionary
+        and save the string to the file specified
+    - reload(self): this will read the string in the file and then to __objects on if the file exists
+
+After creating the class then the next step is to make it available
+to all the classes created in the project
+This is made possible by creating an instance of the class in the __init__.py file
+
+
 """
