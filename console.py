ed5e5c7881aaa1e835e54cafa6c6ec4fe85c85de
HassanMunene <awanzihassan@gmail.com>
2023-04-08 14:17:49 +0300

    How to quit the interpreter by typing EOF or quit

--- console.py
+++ console.py
@@ -8,3 +8,25 @@ It implements the following:
 The code should not be executed when imported. This means
 that the if __name__ clause must be included
 """
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+    """
+    The subclass of Cmd
+    """
+    prompt = '(hbnb) '
+
+    def do_quit(self, line):
+        """
+        Quit command to exit the program
+        """
+        return True
+    def do_EOF(self, line):
+        """
+        quit the program when typed
+        """
+        return True
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
