122e632d6aee9bf770b9b9ba04c5744a7e34adef
HassanMunene <awanzihassan@gmail.com>
2023-04-08 14:06:58 +0300

    documantaion of the console.py

--- console.py
+++ console.py
@@ -1,1 +1,10 @@
-i
+#!/usr/bin/python3
+"""
+this module contains the entry point of the command interpreter
+It implements the following:
+    - quit of EOF to the exit the program
+    - help (this provided by default by cmd)
+    - custom prompt (hbnb)
+The code should not be executed when imported. This means
+that the if __name__ clause must be included
+"""
